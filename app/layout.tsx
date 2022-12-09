f3815eb3a6e1624b44ca8a6eea0c4a52ab1dcdae
Thabish <kadertabish2@gmail.com>
2022-12-09 14:06:29 +0400

    header-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import { Header } from "./Header";
 
 export default function RootLayout({
 	children,
@@ -8,7 +9,10 @@ export default function RootLayout({
 	return (
 		<html>
 			<head />
-			<body>{children}</body>
+			<body>
+				<Header />
+				<div>{children}</div>
+			</body>
 		</html>
 	);
 }
